6b4979b853e131c92013e6a4962cac2a68cba2ae
Thiago Pontes <thiagopnts@gmail.com>
2011-12-26 18:18:00 -0300

    Generic stack implementation

--- stack.h
+++ stack.h
@@ -1,5 +1,3 @@
-#ifndef STACK_H
-#define STACK_H
 #include <stdlib.h>
 #include <string.h>
 
@@ -14,14 +12,40 @@ typedef struct {
     int allocLength;
 } stack;
 
-static void stackGrow(stack *s);
 
-void *stackNew(stack *s, int size);
+static void stackGrow(stack *s)
+{
+    s->allocLength *= 2;
+    s->elems = realloc(s->elems, s->allocLength*s->elemSize);
+}
 
-void *stackDispose(stack *s);
+void *stackNew(stack *s, int size)
+{
+    assert(s->elemSize > 0);
+    s->elemSize = size;
+    s->logLength = 0;
+    s->elems = malloc(4*size);
+    assert(s->elems != NULL);
+}
 
-void stackPush(stack *s, void *elemAddr);
+void *stackDispose(stack *s)
+{
+    free(s->elems);
+}
 
-void stackPop(stack *s, void *elemAddr);
+void stackPush(stack *s, void *elemAddr)
+{
+    if(s->logLength == s->allocLength) {
+        stackGrow(s);
+    }
+    void *target = (char *) s->elems + s->logLength * s->elemSize;
+    memcpy(target, elemAddr, s->elemSize);
+    s->logLength++;
+}
 
-#endif
+void stackPop(stack *s, void *elemAddr)
+{
+    void *source = (char *) s->elems + (s->logLength-1) * s->elemSize;
+    memcpy(elemAddr, source, s->elemSize);
+    s->logLength--;
+}
